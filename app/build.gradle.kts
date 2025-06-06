29e24294d9fe8b9c167962013f4a681b8b5296b4
ciluuu <monneret5lucie@gmail.com>
2025-06-06 08:53:11 +0200

    merge sftp_connection, student, teacher lucie

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,9 +3,9 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.kapt")
     id("org.jetbrains.kotlin.plugin.compose")
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    //alias(libs.plugins.android.application)
+    //alias(libs.plugins.kotlin.android)
+    //alias(libs.plugins.kotlin.compose)
 }
 
 android {
